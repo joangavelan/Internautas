6196e471888127b0b578b9f422fad714037c6d08
Joan Gavelán <joangavelan@gmail.com>
2021-08-24 19:01:15 -0500

    pro

--- src/sections/Aliados.js
+++ src/sections/Aliados.js
@@ -8,7 +8,7 @@ const { colors } = theme
 
 const Container = styled.section`
   min-height: 100vh;
-  background: url('images/pesca en el mar de chimbote.jpg') no-repeat center;
+  background: url('images/pesca en el mar de chimbote.jpg') no-repeat bottom;
   background-size: cover;
   padding: 10rem 3rem;
 `
